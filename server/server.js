0fa9da4554127445b858a62238f05c272c4dfc0b
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-06-28 17:36:34 +0900

    testing byteArray print

--- server/server.js
+++ server/server.js
@@ -70,26 +70,18 @@ function getConnection(connName){
     }
   }
 
+  //print res data, data type : byteArray
   function readData(data){
+    data = new Uint8Array([data]);
     console.log(data);
   }
-//readData함수 만들어야함
 
 app.get("/api/hello", (req,res) => {
     var server = getConnection("hanium");
-    // var data = {
-    //     "startID" : 0x02,
-    //     "cmd" : 0xff,
-    //     "dataType" : 0xa0,
-    //     "dataSize" : 5
-    // };함
-    // var x = new Int8Array([0x02,0x12,0x01,0x05])
-    // console.log(x)
-    //[0x02, 0x12, 0xff, 0x01, 0x12,0xff,0x00,0x05]
-    var buf = new Buffer.from([0X13,0x12,0xf9,0xa0,0x08])
+    var buf = new Buffer.from([0X12,0x12,0xf9,0xa0,0x08])
     console.log(buf);
     writeData(server,buf);
     server.on('data', function(data) {
-      readData(data.toString());
+      readData(data);
     });
 })
