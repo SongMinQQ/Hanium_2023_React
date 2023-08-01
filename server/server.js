74b4b025b6b8be0946f2400eeaf42c890d62548f
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-08-01 14:04:21 +0900

    8월1일 최종테스트 결과 push

--- server/server.js
+++ server/server.js
@@ -45,8 +45,8 @@ function getConnection(connName){
       console.log(connName + ' Connected: ');
       console.log('   local = %s:%s', this.localAddress, this.localPort);
       console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
-      this.setTimeout(500);
-      this.setEncoding('utf8');
+      this.setTimeout(500);//no reply to server after 0.5 seconds, timeout
+      this.setEncoding('utf8');//encoding reply data
       this.on('data', function(data) {
         console.log(connName + " From Server: " + data.toString());
         this.end();
@@ -68,14 +68,14 @@ function getConnection(connName){
     return client;
   }
 
-//write function, if status is 'drain' recursive call, save data
+//write function, if socket buffer is full, change status to 'drain', recursive call
 
   function writeData(socket, data){
-    var success = !socket.write(data);
+    var success = !socket.write(data);//socket buffer is full, success = false
     if (!success){
       (function(socket, data){
         socket.once('drain', function(){
-          writeData(socket, data);
+          writeData(socket, data);//until buffer is full.. wait
         });
       })(socket, data);
     }
@@ -86,19 +86,22 @@ function getConnection(connName){
   app.get("/api/hello", (req,res) => {
     var args = process.argv;
     console.log(args[2]);
+    let argsSize = args[2].length;
     var server = getConnection("hanium");
     let dataSize = Buffer.allocUnsafe(4);  // Init buffer without writing all data to zeros
-    dataSize.writeInt32LE(0x00000005,0);  // Little endian this time..
+    dataSize.writeInt32LE(argsSize,0);  // Little endian this time..
     var buf = new Buffer.from([0x13,0x12,0xff,0xa0,...dataSize]);
     var buf2 = new Buffer.from(args[2],'ascii');
+    var buf3 = new Buffer.from([0x13,0x12,0xf9,0xa0,0x00,0x00,0x00,0x00]);
     writeData(server,buf);
     setTimeout(() => {
       writeData(server, buf2);
+      //writeData(server, buf3); //또다시 버퍼를 기다리는게 맞는지 확인, 10초마다 재연결? 10초마다 write?
+      //server.end(); //데이터를 보낸 후 통신 강제 종료, Command Receive Error
     }, 2000);
     // server.on('data', function(data) {           //서버가 응답을 보낼 경우 웹에 전송(현재 미사용)
     //   console.log(data);
     //  res.send(data);
     // });
-    //server.end();
     res.send(false);//웹 반응 테스트용, 나중에 서버에서 데이터가 잘 넘어오면 삭제 예정
   })
